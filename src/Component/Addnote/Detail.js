f995655a6feaa91af7added6dcd5101b831abb67
Shivani Yadav <shivani16btcs0413@gmail.com>
2020-11-10 15:15:36 +0530

    detail page changes

--- src/Component/Addnote/Detail.js
+++ src/Component/Addnote/Detail.js
@@ -4,8 +4,74 @@ const Detail = (props)=>{
    
     
  return (
-   <div>
-        <h1>I'm Detail</h1>
+   <div className="Detail_page">
+        <p className="Inspired-by-the-worl">
+            " Inspired by the world and everything it has to offer "
+        </p>
+        <p className="Lorem-ipsum-dolor-si">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eros sit amet
+         sem viverra porttitor vel quis justo. Sed tempus, lorem suscipit vulputate mollis, 
+         mi dolor bibendum mi, non auctor nisi est nec nunc <span className="Lorem-text-style-1">More</span>
+        </p>
+        <p className="Industrys">Industrys</p>
+        <div >
+        <div className="freelancer_Rectangle "> <div className="Freelancer"> Web Development</div></div>
+        <div className="freelancer_Rectangle "> <div className="Freelancer">Fine Art </div></div>
+        </div>
+          <br>
+          </br>
+          <br>
+          </br>
+        <p className="Industrys"><span>Organizations</span></p>
+        <div>
+        <div className="Job_Rectangle "> <div className="Job">IAW </div></div>
+        <div className="Job_Rectangle "> <div className="Job">Denver Rugby  </div></div>
+        </div>
+        <br>
+          </br>
+          <br>
+          </br>
+        <p className="Industrys"><span>Interest</span></p>
+        <div>
+        <div className="enterpreneur_Rectangle"> <div className="enterpreneur">Hiking </div></div>
+        <div className="enterpreneur_Rectangle "> <div className="enterpreneur">Professional Speaking </div></div>
+        <div className="enterpreneur_Rectangle "> <div className="enterpreneur"> Guitar </div></div>
+        </div>
+        <br>
+          </br>
+          <br>
+          </br>
+        <p className="Industrys"><span>Alumuni</span></p>
+        <div>
+        <div className="mompreneur_Rectangle "> <div className="mompreneur">Spar colorodo </div></div>
+        <div className="mompreneur_Rectangle "> <div className="mompreneur">UC Technology </div></div>
+        </div>
+        <br>
+          </br>
+          <br>
+          </br>
+        <p className="Industrys"><span>Languages</span></p>
+        <div>
+        <div className="Intership_Rectangle"> <div className="Internship">Hindi </div></div>
+        <div className="Intership_Rectangle"> <div className="Internship">English </div></div>
+        <div className="Intership_Rectangle "> <div className="Internship"> Spain </div></div>
+        </div>
+        <br>
+          </br>
+          <br>
+          </br>
+        
+     <div className="border_box_exchange">
+     <p className="exchange-text">
+          Exchange Phone call and Email
+     </p>
+
+
+        
+
+        
+     </div>
+     
    </div>
     )
 }
